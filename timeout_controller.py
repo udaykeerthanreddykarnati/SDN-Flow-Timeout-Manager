72718840e03ee7949ee9b9ac0f6d7e94d6efe4d8
udayk <udaykeerthanreddykarnati@gmail.com>
2026-04-16 21:23:16 +0530

    Add files via upload

---
+++ timeout_controller.py
@@ -0,0 +1,115 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet, ethernet
+
+
+class TimeoutController(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(TimeoutController, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+
+    # Switch connects \E2\86\92 install table-miss rule
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)]
+
+        self.add_flow(datapath, 0, match, actions, idle_timeout=0)
+
+    # Function to add flow rule
+    def add_flow(self, datapath, priority, match, actions, idle_timeout=10):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(
+            ofproto.OFPIT_APPLY_ACTIONS, actions)]
+
+        mod = parser.OFPFlowMod(
+            datapath=datapath,
+            priority=priority,
+            match=match,
+            instructions=inst,
+            idle_timeout=idle_timeout,
+            hard_timeout=0,
+            flags=ofproto.OFPFF_SEND_FLOW_REM  # IMPORTANT
+        )
+
+        datapath.send_msg(mod)
+
+    # Handle incoming packets
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
+
+        # Ignore LLDP packets
+        if eth.ethertype == 35020:
+            return
+
+        dst = eth.dst
+        src = eth.src
+        in_port = msg.match['in_port']
+
+        self.logger.info(f"PACKET_IN: {src} -> {dst} (port {in_port})")
+
+        # Learn MAC address
+        self.mac_to_port[dpid][src] = in_port
+
+        # Decide output port
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        # Install flow if known destination
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(
+                in_port=in_port,
+                eth_src=src,
+                eth_dst=dst
+            )
+
+            # CORE FEATURE: timeout-based flow
+            self.add_flow(datapath, 1, match, actions, idle_timeout=10)
+
+        # Send packet out
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=msg.buffer_id,
+            in_port=in_port,
+            actions=actions,
+            data=data
+        )
+
+        datapath.send_msg(out)
+
+    # Flow removal event (timeout happened)
+    @set_ev_cls(ofp_event.EventOFPFlowRemoved, MAIN_DISPATCHER)
+    def flow_removed_handler(self, ev):
+        msg = ev.msg
+        self.logger.info(
+            f"FLOW EXPIRED: match={msg.match}, duration={msg.duration_sec}s"
+        )
